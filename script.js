217f7b4f5ad1ba1d5a67cd75f841f0da91956be7
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-03-03 20:12:40 +0100

    reduce font size of password match message

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function checkSimilarity () {
     
       if (password1.value === password2.value) {
         newPara.textContent = "Password Match ✓";
-        newPara.style.cssText = "color:green; margin: 0; padding: 0; position: absolute; left: 50px; ";
+        newPara.style.cssText = "color:green; margin: 0; padding: 0; position: absolute; left: 50px; font-size: 12px; ";
         rightform.appendChild(newPara);
         inputPassword.forEach((pass) => {
             pass.style.border = '1px solid green';
@@ -21,7 +21,7 @@ function checkSimilarity () {
       } else {
         newPara.textContent = "Passwords do not Match";
         newPara.style.cssText =
-          "color:red; margin: 0; padding: 0; position: absolute; left: 50px; ";
+          "color:red; margin: 0; padding: 0; position: absolute; left: 50px; font-size: 12px;";
           inputPassword.forEach((pass) => {
             pass.style.border = '1px solid red';
         })
